e8163455e15e8b2ff6b65ab8cc75e795076603e3
BenUsername <16494454+BenUsername@users.noreply.github.com>
2024-10-24 12:52:08 +0300

    playwright buidlpack

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: hypercorn app:app --workers 4 --bind 0.0.0.0:$PORT
-release: bash build.sh
--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Install Python dependencies
-pip install -r requirements.txt
-
-# Install Playwright browsers
-playwright install chromium
